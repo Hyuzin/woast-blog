8efaee17c334419bac38de2efef4c50282d63c3b
Hyuzin <muhammad.libran1@gmail.com>
2023-05-30 09:26:16 +0700

    Release

--- api/index.js
+++ api/index.js
@@ -3,51 +3,40 @@ import postRoutes from "./routes/posts.js";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import cookieParser from "cookie-parser";
-import cors from 'cors'
+import cors from "cors";
 import multer from "multer";
 
 const app = express();
 const PORT = 3002;
 
 const corsOptions = {
-  origin: 'http://woast-blog-production.up.railway.app',
+  origin: "http://woast-blog-production.up.railway.app",
   credentials: true,
-}
+};
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", corsOptions.origin[0]);
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  next();
-});
-
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, '../client/public/upload')
+    cb(null, "../client/public/upload");
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now()+file.originalname)
-  }
-})
+    cb(null, Date.now() + file.originalname);
+  },
+});
 
-const upload = multer({ storage })
+const upload = multer({ storage });
 
+app.get("/test", (req, res) => {
+  res.json({ message: "Hello, World" });
+});
 
-app.post('/api/upload', upload.single('file'), function (req, res) {
+app.post("/api/upload", upload.single("file"), function (req, res) {
   const file = req.file;
-  res.status(200).json(file.filename)
-})
+  res.status(200).json(file.filename);
+});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
